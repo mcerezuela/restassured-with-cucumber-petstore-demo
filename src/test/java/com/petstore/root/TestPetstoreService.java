95acdb8c943367afeb90e2cb2dca86778a43674b
marcusalten <marcus.cerezuela@alten.es>
2021-06-01 14:19:16 +0200

    renamed some variables

--- src/test/java/com/petstore/root/TestPetstoreService.java
+++ src/test/java/com/petstore/root/TestPetstoreService.java
@@ -3,6 +3,7 @@ package com.petstore.root;
 
 
 import com.petstore.common.CommonUtils;
+import com.petstore.entities.endpoints.AllPetsWrapper;
 import com.petstore.entities.endpoints.EndPointsWrapper;
 import com.petstore.PetStoreUtils;
 import com.petstore.components.schemas.pet.PetResponseEntity;
@@ -12,6 +13,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,12 +23,14 @@ public class TestPetstoreService {
     Petstore referencePetStore;
     EndPointsWrapper endPointsWrapper;
     CommonUtils commonUtils;
-    List<Parameter> parameters;
-    PetResponseEntity[] AllPets;
+    List<Parameter> byStatusParameters;
+    List<Parameter__1> byTagParameters;
+    AllPetsWrapper allPetsList;
 
     public TestPetstoreService() {
         try {
             referencePetStore = PetStoreUtils.getReferencePetStore();
+            allPetsList = new AllPetsWrapper();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,6 +46,7 @@ public class TestPetstoreService {
     public void getEndpoints(){
         endPointsWrapper = PetStoreUtils.extractEndPoints(referencePetStore.getPaths());
     }
+
 
     @Test
     public void requestOpenAPIFromService() {
@@ -70,41 +75,62 @@ public class TestPetstoreService {
     public void getPetsByStatusDefault() {
         String parameterName = getPetFindByStatusParameters().get(0).getName();
         String parameterValue = getPetFindByStatusParameters().get(0).getSchema().getDefault();
-        PetResponseEntity[] petReceived = getPetsBystatus(parameterName,parameterValue);
-        Assert.assertNotNull(petReceived);
-    }
-
-    @Test
-    public void getPetsByStatusPending() {
-        String parameterName = getPetFindByStatusParameters().get(0).getName();
-        String parameterValue = getPetFindByStatusParameters().get(0).getSchema().getEnum().get(1);
-        PetResponseEntity[] petReceived = getPetsBystatus(parameterName,parameterValue);
+        PetResponseEntity[] petReceived = getRequestPetsBystatus(parameterName,parameterValue);
         Assert.assertNotNull(petReceived);
     }
 
     @Test
     public void getPetsByStatusAvailable() {
-        String parameterName = getPetFindByStatusParameters().get(0).getName();
-        String parameterValue = getPetFindByStatusParameters().get(0).getSchema().getEnum().get(0);
-        PetResponseEntity[] petReceived = getPetsBystatus(parameterName,parameterValue);
+        PetResponseEntity[] petReceived = getPetsBystatus(0);
+        Assert.assertNotNull(petReceived);
+    }
+
+    @Test
+    public void getPetsByStatusPending() {
+        PetResponseEntity[] petReceived = getPetsBystatus(1);
         Assert.assertNotNull(petReceived);
     }
 
     @Test
     public void getPetsByStatusSold() {
-        String parameterName = getPetFindByStatusParameters().get(0).getName();
-        String parameterValue = getPetFindByStatusParameters().get(0).getSchema().getEnum().get(2);
-        PetResponseEntity[] petReceived = getPetsBystatus(parameterName,parameterValue);
+        PetResponseEntity[] petReceived = getPetsBystatus(2);
         Assert.assertNotNull(petReceived);
     }
 
-    private PetResponseEntity[] getPetsBystatus(String parameterName, String parameterValue) {
-        String PetFindByStatusEndpoint = endPointsWrapper.getEndpointValue("PetFindByStatus");
+    @Test
+    public void getPetsByTags() {
+        requestAllPetsAvailability();
+        sendRequestsForPetsByTags(allPetsList.getListOfPetsWithNonCeroTags());
+    }
+
+    private void sendRequestsForPetsByTags(ArrayList<PetResponseEntity> allPetsWithTags) {
+        String parameterName = getPetPetFindByTags().get(0).getName();
+        for(PetResponseEntity petResponseEntity :allPetsWithTags){
+            Assert.assertNotNull(senGetRequestQuery(parameterName,petResponseEntity.getTags()
+            .get(0).getName(),"petFindByTags"));
+        }
+
+    }
+
+    private PetResponseEntity[] getPetsBystatus(int statusIndex) {
+        String parameterName = getPetFindByStatusParameters().get(0).getName();
+        String parameterValue = getPetFindByStatusParameters().get(0).getSchema().getEnum()
+                .get(statusIndex);
+        return getRequestPetsBystatus(parameterName,parameterValue);
+    }
+
+    private PetResponseEntity[] getRequestPetsBystatus(String parameterName, String parameterValue) {
+        return senGetRequestQuery(parameterName, parameterValue,"PetFindByStatus");
+    }
+
+    private PetResponseEntity[] senGetRequestQuery(String parameterName, String parameterValue,
+                                                   String operationId) {
+        String endpoint = endPointsWrapper.getEndpointValue(operationId);
         return given().
                 spec(commonUtils.getSpecs()).
                 queryParam(parameterName, parameterValue).
                 when().
-                get(PetFindByStatusEndpoint).
+                get(endpoint).
                 then().
                 statusCode(200).
                 extract().
@@ -112,10 +138,23 @@ public class TestPetstoreService {
     }
 
     private List<Parameter> getPetFindByStatusParameters() {
-        if(null==parameters){
-            parameters = referencePetStore.getPaths().getPetFindByStatus().getGet().getParameters();
+        if(null== byStatusParameters){
+            byStatusParameters = referencePetStore.getPaths().getPetFindByStatus().getGet().getParameters();
         }
-        return parameters;
+        return byStatusParameters;
+    }
+
+    private List<Parameter__1> getPetPetFindByTags() {
+        if(null== byTagParameters){
+            byTagParameters = referencePetStore.getPaths().getPetFindByTags().getGet().getParameters();
+        }
+        return byTagParameters;
+    }
+
+    private void requestAllPetsAvailability() {
+        allPetsList.add(getPetsBystatus(0));
+        allPetsList.add(getPetsBystatus(1));
+        allPetsList.add(getPetsBystatus(2));
     }
 
 }
